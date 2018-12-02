516b922add29442d6c7d28c496e42c450023ac65
davidccgarcia <ccristhiangarcia@gmail.com>
2018-12-02 08:42:38 -0500

    Paginate records of the notes

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -13,7 +13,7 @@ class NotesController extends Controller
 {
     public function index()
     {
-        $notes = Note::all();
+        $notes = Note::paginate();
         return view('notes.list', compact('notes'));
     }
 
--- resources/views/notes/list.blade.php
+++ resources/views/notes/list.blade.php
@@ -11,4 +11,5 @@
             </li>
         @endforeach
     </ul>
+    {!! $notes->render() !!}
 @endsection
